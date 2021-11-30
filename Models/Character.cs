796afb7e8de1c8ff0b33f42b4ad06f96bddd5b5e
Christoper Reader <christopher.reader@linnworks.com>
2021-11-30 16:00:55 +0000

    Change default character to humamn

--- Models/Character.cs
+++ Models/Character.cs
@@ -3,11 +3,12 @@ namespace dotnet_rpg.Models
     public class Character
     {
         public int Id { get; set; }
-        public string Name { get; set; } = "Frodo";
+        public string Name { get; set; } = "Aragorn";
         public int HitPoints { get; set; } = 100;
         public int Strength { get; set; } = 10;
         public int Defense { get; set; } = 10;
         public int Intelligence { get; set; } = 10;
         public RpgClass Class { get; set; } = RpgClass.Knight;
+        public RpgRace Race { get; set; } = RpgRace.Human;
     }
 }
